0f69d42635eea72b03f8cbdbee3cd338fddf0566
Ericw <eweissm1@asu.edu>
2024-11-18 20:14:04 -0700

    New Commit

---
+++ Main.py
@@ -0,0 +1,129 @@
+#TODO:
+# FR and coiling will randomly stop early
+
+import cv2
+#from matplotlib import pyplot as plt
+import numpy as np
+import serial
+import time
+
+global ser
+global arduinoQueue
+global localQueue
+
+
+################################################################################################
+## Define some functions
+################################################################################################
+
+
+def packAndSendMsg(Command):
+    #Packs together our message, taking the command character and the text entries and sends it over serial
+    global ser
+    print(Command)
+    Parameters = ReadInputs()
+    msg = Command  # Add command indicator to msg
+
+    for i in Parameters:
+        msg = msg + ',' + i  # add the parameters to the message
+
+    msg = msg + 'Z'  # add end of message indicator
+
+    ser.write(bytes(str(msg), 'UTF-8'))
+
+# arduinoQueue = queue.Queue()
+# localQueue = queue.Queue()
+
+# def listenToArduino():
+#     message = b''
+#     while True:
+#         incoming = ser.read()
+#         if (incoming == b'\n'):
+#             arduinoQueue.put(message.decode('utf-8').strip().upper())
+#             message = b''
+#         else:
+#             if ((incoming != b'') and (incoming != b'\r')):
+#                  message += incoming
+
+
+ser = serial.Serial(port= 'COM4', baudrate=9600, timeout=10)  # create Serial Object, baud = 9600, read times out after 10s
+time.sleep(.1)  # delay 3 seconds to allow serial com to get established
+print("Connected")
+
+
+################################################################################################
+## Robot Controls
+################################################################################################
+#connect camera
+video_0 = cv2.VideoCapture(0)
+
+#specify color HSV bounds
+lower_bound = np.array([10, 0,245])
+upper_bound = np.array([200, 10, 255])
+
+while(True):
+
+#get video frames
+    retu, frame = video_0.read()
+
+#get hsv colors
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+#find color masks
+    mask = cv2.inRange(hsv, lower_bound, upper_bound)
+
+    # define kernel size
+    kernel = np.ones((7, 7), np.uint8)
+
+    # Remove unnecessary noise from mask
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+
+
+    color1 = cv2.bitwise_and(frame, frame, mask=mask)
+
+
+#code to get specific color
+    # x = 100
+    # y = 100
+    # colorsB = frame[y,x,0]
+    # colorsG = frame[y,x,1]
+    # colorsR = frame[y,x,2]
+    # colors = frame[y,x]
+    # hsv_value= np.uint8([[[colorsB ,colorsG,colorsR ]]])
+    # hsv = cv2.cvtColor(hsv_value,cv2.COLOR_BGR2HSV)
+    # print ("HSV : " ,hsv)
+    #
+    # # Print the HSV value
+    #
+    # frame[x-5:x+5, y-5:y+5] = [0, 255, 0]
+
+    #finds contours from colors
+    contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+#array of center points of contours
+    C = np.empty([len(contours), 2], 'i')
+
+    # Draw contour on original image
+    output = cv2.drawContours(frame, contours, -1, (0, 0, 255), 2)
+
+#finds centerpoint of colored dots... adds to array and adds dot to image
+    if len(contours) > 0:
+        for i in range(len(contours)):
+            M = cv2.moments(contours[i])
+            C[i, 0] = int(M['m10'] / M['m00'])  # cx
+            C[i, 1] = int(M['m01'] / M['m00'])  # cy
+            output[C[i, 1] - 2:C[i, 1] + 2, C[i, 0] - 2:C[i, 0] + 2] = [255, 255, 255]
+            output = cv2.putText(output, str(i), (C[i, 0], C[i, 1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (245, 244, 66),2, cv2.LINE_AA)
+        print([C[0, 0],C[0, 1]])
+
+    if len(contours) > 1:
+        print("Error: Multiple Centers")
+        cv2.line(output, (C[0,0],C[0,1]),(C[1,0],C[1,1]), (255, 0, 0), 2 )
+
+    cv2.imshow('Output', output)
+
+    if cv2.waitKey(1) & 0xFF==ord('a'):
+        break
+video_0.release()
+cv2.destroyAllWindows()
